f0bb7abb7ac69d79b9d7ddeadb8b8a6cf8100252
riz-ujjal <rizujjal@gmail.com>
2026-07-08 23:36:08 +0530

    add .lower() directly here and also added new function add task so it takes care of speach to text for the user's tasks

--- Task-1/Listening_to_user.py
+++ Task-1/Listening_to_user.py
@@ -1,9 +1,29 @@
 import speech_recognition as sr
-import pyaudio
+recognizer = sr.Recognizer()
 def Listen():
-    recognizer = sr.Recognizer()
+    
     with sr.Microphone(device_index=2) as source:
+        recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
         message = recognizer.recognize_google(audio)
+        message = message.lower()
+        print(message)
     return message
 Listen()
+
+def Add_Task():
+    try :
+        with sr.Microphone(device_index=2) as source:
+            recognizer.adjust_for_ambient_noise(source)
+            audio = recognizer.listen(source)
+
+            message = recognizer.recognize_google(audio)
+            message = message.lower()
+            print(message)
+        return message
+    except recognizer.UnknownValueError():
+        message = f"Couldn't hear you clear"
+        return message
+        
+
+Add_Task()
