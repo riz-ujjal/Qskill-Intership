95daa7ced29bd8201e44fc78611c406a772742dc
riz-ujjal <rizujjal@gmail.com>
2026-07-08 23:34:40 +0530

    just remove the lower convertion and added it to the listening file

--- Task-1/main.py
+++ Task-1/main.py
@@ -2,25 +2,31 @@ from Weather import get_weather
 from News_Reader import News_Reading
 from Listening_to_user import Listen
 from Speaking_to_user import Speak
+from Task_setting import Task
 
 while True :
     command = Listen()
     print(command)
-    if "stop" in command.lower() or "good bye" in command.lower() or "goodbye" in command.lower():
+    if "stop" in command or "good bye" in command  or "goodbye" in command or "no" in command :
         Speak("GoodBye shutting down!")
         break
     
-    elif "Hello" in command.lower():
+    elif "Hello" in command :
         Speak("Hello Ueser")
+        break
 
-    elif "weather" in command.lower() or "temperature" in command.lower():
+    elif "weather" in command  or "temperature" in command :
         Speak("Let me check the temperature for today...")
         Speak("searching for your location...")
         Speak(get_weather())
+        break
     
-    elif "news" in command.lower():
+    elif "news" in command :
         Speak("Here are top two news for today!")
         Speak(News_Reading())
+        break   
+    
+
     
         
 
