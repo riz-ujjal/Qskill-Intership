f7827c9f6b7dbd5f40fe642aa723e4e1ca98094a
riz-ujjal <rizujjal@gmail.com>
2026-07-09 15:19:55 +0530

    changing the files

--- Task-1/Speaking_to_user.py
+++ Task-1/Speaking_to_user.py
@@ -1,5 +1,6 @@
 import pyttsx3
-
+# from Task_setting import Add_task
+# text = Add_task
 def Speak(text):
     engine = pyttsx3.init()
     engine.say(text)
--- Task-1/Task_setting.py
+++ Task-1/Task_setting.py
@@ -1,16 +1,10 @@
-from Listening_to_user import Listen
-from Speaking_to_user import Speak
-message =Listen()
-def Add_task():
-    
-    if "add" in message :
-            text = message.replace("add", "").strip()
 
-            if text != "":
-                with open("Demo.txt", "a") as file :
-                    file.write(text + "\n")
-                    Speak( f"Added {text}")
+def Add_task(message):
+    text = message.replace("add", "").strip()       
+    if text != "":
+        with open("Demo.txt", "a") as file :
+            file.write(text + "\n")
+            return( f"Added {text}")
 
-            else :
-                Speak("Did't catch say it again!")
-Add_task()
+    else :
+        return("Did't catch say it again!")
