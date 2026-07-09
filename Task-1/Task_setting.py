1c88caafc79d9fd800fdd0e10bdcc5880a67621b
riz-ujjal <rizujjal@gmail.com>
2026-07-09 12:30:16 +0530

    successfully done with the task part

--- Task-1/Task_setting.py
+++ Task-1/Task_setting.py
@@ -1,16 +1,16 @@
 from Listening_to_user import Listen
 from Speaking_to_user import Speak
-
+message =Listen()
 def Add_task():
     
-    message = Listen()
     if "add" in message :
             text = message.replace("add", "").strip()
+
             if text != "":
                 with open("Demo.txt", "a") as file :
                     file.write(text + "\n")
                     Speak( f"Added {text}")
+
             else :
-                return "Did't catch say it again!"
-        
-Speak(Add_task())
+                Speak("Did't catch say it again!")
+Add_task()
