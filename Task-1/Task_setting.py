b955acb645b3cb7a7ea141cc2f555bb47f636840
riz-ujjal <rizujjal@gmail.com>
2026-07-09 12:03:17 +0530

    checking things up

--- Task-1/Task_setting.py
+++ Task-1/Task_setting.py
@@ -1,21 +1,16 @@
-from Listening_to_user import Add_Task
+from Listening_to_user import Listen
 from Speaking_to_user import Speak
 
-def Task():
-    message = Add_Task()
-    while True:
+def Add_task():
     
-        if "done" in message:
-            Speak("ok We are Done")
-            break
-
-        elif "add" in message :
+    message = Listen()
+    if "add" in message :
             text = message.replace("add", "").strip()
             if text != "":
                 with open("Demo.txt", "a") as file :
                     file.write(text + "\n")
+                    Speak( f"Added {text}")
             else :
                 return "Did't catch say it again!"
-        return "Task  added successfully!"
-Task()
-Speak(Task())
+        
+Speak(Add_task())
