fe8fa1586a943b3b4138d0b5baa9d8d6ef7f74c6
riz-ujjal <rizujjal@gmail.com>
2026-07-17 13:56:18 +0530

    completed the task 2

--- Task-2/plotter.py
+++ Task-2/plotter.py
@@ -1,8 +1,30 @@
 import matplotlib.pyplot as plt
 
 def plot_bar_chart(df):
-    pass
+    df['Muscle_Group']
+    x = df['Muscle_Group']
+    df['Calories_Burned']
+    y = df['Calories_Burned']
+    plt.title("compare")
+    plt.xlabel('X Label')
+    plt.ylabel('Y Label')
+    plt.bar(x,y)
+    plt.show()
+    
 def plot_scatter(df):
-    pass
+    df['Avg_Heart_Rate']
+    df['Calories_Burned']
+    x = df['Avg_Heart_Rate']
+    y = df['Calories_Burned']
+    plt.scatter(x,y)
+    plt.title("compare")
+    plt.xlabel('X Label')
+    plt.ylabel('Y Label')
+    plt.bar(x,y)
+    plt.show()
+    
 def plot_heatmap(df):
-    pass
+    matrix = df[['Duration_Minutes', 'Calories_Burned', 'Avg_Heart_Rate']].corr()
+    plt.imshow(matrix, cmap='viridis')
+    plt.colorbar()
+    plt.show()
